7a5341310cde22feb145ea6c9029f1ba37695703
i2nerd4u <karl360@live.ie>
2025-12-12 23:40:28 +0000

    test approval workflow

--- calorie_counter/app.py
+++ calorie_counter/app.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
             counter.reset_day()
             print("Day reset.")
         elif choice == "4":
-            print("Exiting application, goodbye! test")
+            print("Exiting application, goodbye! test approval workflow")
             break
